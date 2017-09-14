0de302388db8e58b315c9a726824ea410e3f302e
Afton Mower <affyqm@gmail.com>
2017-09-14 20:41:44 +0000

    Comment out remaining attr_accessors in file

--- lib/models_v2.rb
+++ lib/models_v2.rb
@@ -14,8 +14,6 @@ DB = Sequel.connect('mysql://portfolio_god:password@localhost/hs_portfolio')
 class Account < Sequel::Model #dataset for DB[:accounts]
     #attr_accessor :account_id, :acct_first_name, :acct_last_name, :zipcode, :email, :password, :join_date, :account_hash
     
-    
-    
     one_to_many :students
     one_to_many :activities
     one_to_many :books
@@ -23,7 +21,7 @@ class Account < Sequel::Model #dataset for DB[:accounts]
 end
 
 class Activity < Sequel::Model #dataset for DB[:activities]
-    attr_accessor :activity_id, :title, :duration, :description, :activity_date, :activity_slug, :account_id
+    #attr_accessor :activity_id, :title, :duration, :description, :activity_date, :activity_slug, :account_id
     
     include Comparable
     one_to_many :activities_students
@@ -35,21 +33,21 @@ end
 
 #I'm not sure this class is named correctly
 class ActivitiesStudent < Sequel::Model #dataset for DB[:activities_students]
-    attr_accessor :activities_students_id, :activity_id, :student_id, :account_id
+    #attr_accessor :activities_students_id, :activity_id, :student_id, :account_id
     
     many_to_one :activity
     many_to_one :student
 end
 
 class ActivitiesSubject < Sequel::Model #dataset for DB[:activities_subjects]
-    attr_accessor :activities_subjects_id, :activity_id, :subject_id, :account_id
+    #attr_accessor :activities_subjects_id, :activity_id, :subject_id, :account_id
     
     many_to_one :activity
     many_to_many :subject
 end
 
 class Book < Sequel::Model #dataset for DB[:books]
-    attr_accessor :book_id, :title, :author, :rating, :finish_date, :book_slug, :account_id, :fiction, :nonfiction
+    #attr_accessor :book_id, :title, :author, :rating, :finish_date, :book_slug, :account_id, :fiction, :nonfiction
 
     include Comparable
     one_to_many :books_students
@@ -68,21 +66,21 @@ end
 
 #I'm not sure this class is named correctly
 class BooksStudent < Sequel::Model #dataset for DB[:books_students]
-    attr_accessor :books_students_id, :book_id, :student_id, :account_id
+    #attr_accessor :books_students_id, :book_id, :student_id, :account_id
 
     many_to_one :book
     many_to_one :student
 end
 
 class BooksSubject < Sequel::Model #dataset for DB[:books_subjects]
-    attr_accessor :books_subjects_id, :book_id, :subject_id, :account_id
+    #attr_accessor :books_subjects_id, :book_id, :subject_id, :account_id
     
     many_to_one :book
     many_to_one :subject
 end
 
 class Student < Sequel::Model #dataset for DB[:students]
-    attr_accessor :student_id, :account_id, :stud_first_name, :stud_last_name, :birth_date, :enroll_date, :student_slug
+    #attr_accessor :student_id, :account_id, :stud_first_name, :stud_last_name, :birth_date, :enroll_date, :student_slug
 
     many_to_one :account
     one_to_many :activities_students
@@ -90,7 +88,7 @@ class Student < Sequel::Model #dataset for DB[:students]
 end
 
 class Subject < Sequel::Model #dataset for DB[:subjects]
-    attr_accessor :subject_id, :subject_name, :subject_slug, :account_id, :description
+    #attr_accessor :subject_id, :subject_name, :subject_slug, :account_id, :description
 
     many_to_one :account
     many_to_many :activities_subjects
